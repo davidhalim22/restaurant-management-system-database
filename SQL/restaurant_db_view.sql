4dc6e0cb2f11e0d715e03b5c766d5ca980d9941f
davidhalim22 <137604584+davidhalim22@users.noreply.github.com>
2025-12-16 02:59:09 +0700

    Add files via upload

---
+++ SQL/restaurant_db_view.sql
@@ -0,0 +1,10 @@
+USE restaurant_db;
+select * from Table_;
+SELECT * FROM Customer;
+SELECT * FROM Staff;
+SELECT * FROM Menu;
+SELECT * FROM Reservation;
+SELECT * FROM Orders;
+SELECT * FROM Order_Details;
+select * from Membership
+
